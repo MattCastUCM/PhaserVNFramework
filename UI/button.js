42427ffd567feabe06a3da85c614d028e589d6f6
MattCastUCM <mattcast@ucm.es>
2025-07-12 11:10:40 +0200

    omg

--- UI/button.js
+++ UI/button.js
@@ -54,7 +54,7 @@ export default class Button extends InteractiveContainer {
         textPaddingX = 0, textPaddingY = 0, textOffsetX = 0, textOffsetY = 0, textOriginX = 0.5, textOriginY = 0.5, textAlignX = 0.5, textAlignY = 0.5,
         normalTintColor = 0xffffff, hoverTintColor = 0xd9d9d9, pressingTintColor = 0x969696)
     {
-        this.createImgButtonWithAtlas(text, textConfig, onclick, "", img, imgOriginX, imgOriginY, imgScaleX, imgScaleY, imgAlpha, textPaddingX, textPaddingY, 
+        this.createImgButtonWithAtlas(text, textConfig, onClick, "", img, imgOriginX, imgOriginY, imgScaleX, imgScaleY, imgAlpha, textPaddingX, textPaddingY, 
             textOffsetX, textOffsetY, textOriginX, textOriginY, textAlignX, textAlignY, normalTintColor, hoverTintColor, pressingTintColor);
     }
     createImgButtonWithAtlas(text = "", textConfig = {}, onClick = () => { },
